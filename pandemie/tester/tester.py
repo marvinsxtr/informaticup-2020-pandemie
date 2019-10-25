126b041c6c9d5d97a7d246975953aa16f02324e2
nl <nils@langius.de>
2019-10-25 22:29:10 +0200

    added example strategy

--- pandemie/tester/tester.py
+++ pandemie/tester/tester.py
@@ -1,9 +1,19 @@
 from pandemie.tester import AbstractStrategy
 
 
-class Tester(AbstractStrategy):
+class Tester:
+    def __init__(self, strategy):
+        if not isinstance(strategy, AbstractStrategy):
+            raise ValueError("Strategy is not valid.")
+
+
+class ExampleStrategy(AbstractStrategy):
     def __init__(self):
         super().__init__()
 
     def solve(self, json_data):
-        print("DO something")
+        return "{'type': 'endRound'}"
+
+
+if __name__ == "__main__":
+    my_tester = Tester(ExampleStrategy())
