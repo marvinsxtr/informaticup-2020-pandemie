b0c8fb6d1111bbc4f99fc16915c94528edf46ab9
nl <nils@langius.de>
2019-10-26 00:05:16 +0200

    fix for windows

--- pandemie/tester/tester.py
+++ pandemie/tester/tester.py
@@ -3,21 +3,19 @@ import subprocess
 from pandemie.tester import AbstractStrategy
 from pandemie.web import start_server
 
-if os.name == "nt":
-    tool_cmd = "start ../../test/ic20_windows.exe"
-else:
-    #tool_cmd = "../.././ic20_linux"
-    pass
-
 
 class Tester:
     def __init__(self, strategy):
         if not isinstance(strategy, AbstractStrategy):
             raise ValueError("Strategy is not valid.")
 
-        #os.system(tool_cmd)
-        os.chdir("../../test/")
-        subprocess.call("./ic20_linux", shell=True)
+        if os.name == "nt":
+            os.system("start ../../test/ic20_windows.exe")
+
+        else:
+            os.chdir("../../test/")
+            subprocess.call("./ic20_linux", shell=True)
+
         start_server(strategy)
 
 
