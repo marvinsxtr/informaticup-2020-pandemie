178ece62effd53052f174159d2e0a756d0bae4b8
Marvin Sextro <marvin.sextro@gmail.com>
2019-11-06 02:16:53 +0100

    Improved marvin1

--- pandemie/tester/strategies/marvin1.py
+++ pandemie/tester/strategies/marvin1.py
@@ -12,12 +12,29 @@ class Marvin1(AbstractStrategy):
 
         points = json_data["points"]
         round = json_data["round"]
+        cities = json_data["cities"]
 
-        cities = dict()
+        print("solving with marvin1 in round", round)
 
-        for city in json_data["cities"]:
-            cities[city] = 0
-        print(cities)
+        if points < 3:
+            return operations.end_round()
+
+        for key, value in sorted(cities.items(), key=lambda item: get_single_score(item[1]["hygiene"])):
+            if get_single_score(value["hygiene"]) == 1:
+                return operations.apply_hygienic_measures(key)
+
+        for key, value in sorted(cities.items(), key=lambda item: get_single_score(item[1]["awareness"])):
+            if get_single_score(value["awareness"]) == 1:
+                return operations.launch_campaign(key)
+
+        for key, value in sorted(cities.items(), key=lambda item: get_single_score(item[1]["government"])):
+            if get_single_score(value["government"]) == 1:
+                return operations.call_elections(key)
+
+        for key, value in sorted(cities.items(), key=lambda item: get_single_score(item[1]["economy"])):
+            if get_single_score(value["economy"]) == 1:
+                return operations.exert_influence(key)
+
         return operations.end_round()
 
     def get_result(self):
@@ -26,13 +43,13 @@ class Marvin1(AbstractStrategy):
 
 def get_single_score(symbols):
     if symbols == "--":
-        return 0
-    if symbols == "-":
         return 1
-    if symbols == "o":
+    if symbols == "-":
         return 2
-    if symbols == "+":
+    if symbols == "o":
         return 3
-    if symbols == "++":
+    if symbols == "+":
         return 4
+    if symbols == "++":
+        return 5
     print("wrong symbols")
