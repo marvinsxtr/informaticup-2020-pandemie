1ee02879a003d7372b8072e4f1b5f967fde1ca62
Marvin Sextro <marvin.sextro@gmail.com>
2019-11-06 14:42:35 +0100

    Fixed minor bug in express test

--- pandemie/express/index.js
+++ pandemie/express/index.js
@@ -3,7 +3,7 @@ let app = express();
 
 app.use(express.json({ limit: "2mb" }));
 app.post("/", function(request, response) {
-	let { round, outcome, error } = request.body
+	let { round, outcome, error } = request.body;
 	console.log(`Round: ${round}, outcome: ${outcome}, error: ${error}`);
 	response.send({ type: "endRound" });
 });
