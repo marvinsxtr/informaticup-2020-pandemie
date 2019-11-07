07f971f595a2b6fedce34c45251cbf202ec393e3
Marvin Sextro <marvin.sextro@gmail.com>
2019-11-07 22:25:46 +0100

    Collecting data in marvin1

--- pandemie/tester/strategies/marvin1.py
+++ pandemie/tester/strategies/marvin1.py
@@ -9,36 +9,92 @@ class Marvin1(AbstractStrategy):
     def _solve(self, json_data, server):
         if "error" in json_data:
             print(json_data["error"])
+            return operations.end_round()
 
+        cities = json_data["cities"]
+        events = json_data["events"]
         points = json_data["points"]
         round = json_data["round"]
-        cities = json_data["cities"]
+        outcome = json_data["outcome"]
 
-        print("solving with marvin1 in round", round)
+        pathogens = []
+
+        pathogens_med_developing = []
+        pathogens_med_available = []
+
+        for event in events:
+            if event["type"] == "pathogenEncountered":
+                pathogens.append(event["pathogen"])
+
+        pathogens_sorted = sorted(pathogens, key=lambda p: (0.3 * score(p["infectivity"]), 0.2 * score(p["mobility"]),
+                                                            0.2 * score(p["duration"]), 0.3 * score(p["lethality"])),
+                                  reverse=True)
+
+        for event in events:
+            if event["type"] == "medicationInDevelopment":
+                pathogens_med_developing.append(event["pathogen"]["name"])
+                print("available", event["pathogen"]["name"])
+
+        for event in events:
+            if event["type"] == "medicationAvailable":
+                pathogens_med_available.append(event["pathogen"]["name"])
+                print("available", event["pathogen"]["name"])
+
+        for event in events:
+            if event["type"] == "pathogenEncountered":
+                if event["pathogen"]["name"] not in pathogens_med_developing:
+                    if event["pathogen"]["name"] not in pathogens_med_available:
+                        if event["pathogen"]["name"] == pathogens_sorted[0]["name"]:
+                            if operations.PRICES["develop_medication"]["initial"] <= points:
+                                print("develop", event["pathogen"]["name"])
+                                return operations.develop_medication(event["pathogen"]["name"])
+
+        print("round:", round, "points:", points, "outcome:", outcome)
+
+        stats = False
+        if stats:
+            count_events = 0
+            outbreak_events = 0
+            other_events = 0
+            for city in cities.items():
+                city_name = city[0]
+                city_info = city[1]
+
+                if "events" in city_info:
+                    if len(city_info["events"]) > 0:
+                        for event in city_info["events"]:
+                            if event["type"] == "outbreak":
+                                outbreak_events = outbreak_events + 1
+                            else:
+                                other_events = other_events + 1
+                        count_events = count_events + 1
+
+            print("events:", count_events, "outbreaks:", outbreak_events, "others:", other_events,
+                  "cities:", len(cities))
 
         if points < 3:
             return operations.end_round()
 
-        for key, value in sorted(cities.items(), key=lambda item: get_single_score(item[1]["hygiene"])):
-            if get_single_score(value["hygiene"]) == 1:
+        for key, value in sorted(cities.items(), key=lambda item: score(item[1]["hygiene"])):
+            if score(value["hygiene"]) == 1:
                 return operations.apply_hygienic_measures(key)
 
-        for key, value in sorted(cities.items(), key=lambda item: get_single_score(item[1]["awareness"])):
-            if get_single_score(value["awareness"]) == 1:
+        for key, value in sorted(cities.items(), key=lambda item: score(item[1]["awareness"])):
+            if score(value["awareness"]) == 1:
                 return operations.launch_campaign(key)
 
-        for key, value in sorted(cities.items(), key=lambda item: get_single_score(item[1]["government"])):
-            if get_single_score(value["government"]) == 1:
+        for key, value in sorted(cities.items(), key=lambda item: score(item[1]["government"])):
+            if score(value["government"]) == 1:
                 return operations.call_elections(key)
 
-        for key, value in sorted(cities.items(), key=lambda item: get_single_score(item[1]["economy"])):
-            if get_single_score(value["economy"]) == 1:
+        for key, value in sorted(cities.items(), key=lambda item: score(item[1]["economy"])):
+            if score(value["economy"]) == 1:
                 return operations.exert_influence(key)
 
         return operations.end_round()
 
 
-def get_single_score(symbols):
+def score(symbols):
     if symbols == "--":
         return 1
     if symbols == "-":
