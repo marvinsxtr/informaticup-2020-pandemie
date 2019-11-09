ddf0f4c8c9f1842b0e983a96ded48798571422e8
Marvin Sextro <marvin.sextro@gmail.com>
2019-11-09 23:00:04 +0100

    Visualize events

--- pandemie/visualization.py
+++ pandemie/visualization.py
@@ -5,7 +5,6 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import plotly.graph_objects as go
 import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -18,7 +17,7 @@ app.layout = html.Div(
         html.Div(id='game-state'),
         dcc.Interval(
             id='interval-component',
-            interval=10000,  # in millis
+            interval=200,  # in millis
             n_intervals=0
         )
     ])
@@ -41,46 +40,35 @@ def visualize_round_number(json_data):
 
 
 def visualize_connections_infected(json_data):
-    df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_february_us_airport_traffic.csv')
-    df.head()
-
-    df['text'] = df['airport'] + '' + df['city'] + ', ' + df['state'] + '' + 'Arrivals: ' + df['cnt'].astype(str)
-
-    scl = [[0, "rgb(5, 10, 172)"], [0.35, "rgb(40, 60, 190)"], [0.5, "rgb(70, 100, 245)"], \
-           [0.6, "rgb(90, 120, 245)"], [0.7, "rgb(106, 137, 247)"], [1, "rgb(220, 220, 220)"]]
+    lat = []
+    lon = []
+    for city in json_data["cities"].items():
+        if "events" in city[1]:
+            lat.append(city[1]["latitude"])
+            lon.append(city[1]["longitude"])
 
     data = [dict(
         type='scattergeo',
-        locationmode='USA-states',
-        lon=df['long'],
-        lat=df['lat'],
-        text=df['text'],
+        locationmode='natural earth',
+        lon=lon,
+        lat=lat,
         mode='markers',
         marker=dict(
             size=8,
             opacity=0.8,
-            reversescale=True,
-            autocolorscale=False,
-            symbol='square',
+            symbol='circle',
             line=dict(
                 width=1,
                 color='rgba(102, 102, 102)'
             ),
-            colorscale=scl,
-            cmin=0,
-            color=df['cnt'],
-            cmax=df['cnt'].max(),
-            colorbar=dict(
-                title="Incoming flightsFebruary 2011"
-            )
         ))]
 
     layout = dict(
-        title='Most trafficked US airports<br>(Hover for airport names)',
+        title='Events',
         colorbar=True,
         geo=dict(
-            scope='usa',
-            projection=dict(type='albers usa'),
+            scope='earth',
+            projection='earth',
             showland=True,
             landcolor="rgb(250, 250, 250)",
             subunitcolor="rgb(217, 217, 217)",
