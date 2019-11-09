4880276a0344db45bde617e61a2ae43ae64327b7
Marvin Sextro <marvin.sextro@gmail.com>
2019-11-09 18:07:12 +0100

    Simplified operations

--- pandemie/operations.py
+++ pandemie/operations.py
@@ -1,64 +1,62 @@
-import json
-
-PRICES = dict({
-    "end_round": dict({"initial": 0, "each": 0}),
-    "put_under_quarantine": dict({"initial": 20, "each": 10}),
-    "close_airport": dict({"initial": 15, "each": 5}),
-    "close_connection": dict({"initial": 3, "each": 3}),
-    "develop_vaccine": dict({"initial": 40, "each": 0}),
-    "deploy_vaccine": dict({"initial": 5, "each": 0}),
-    "develop_medication": dict({"initial": 20, "each": 0}),
-    "deploy_medication": dict({"initial": 10, "each": 0}),
-    "exert_influence": dict({"initial": 3, "each": 0}),
-    "call_elections": dict({"initial": 3, "each": 0}),
-    "apply_hygienic_measures": dict({"initial": 3, "each": 0}),
-    "launch_campaign": dict({"initial": 3, "each": 0})
-})
+PRICES = {
+    "end_round": {"initial": 0, "each": 0},
+    "put_under_quarantine": {"initial": 20, "each": 10},
+    "close_airport": {"initial": 15, "each": 5},
+    "close_connection": {"initial": 3, "each": 3},
+    "develop_vaccine": {"initial": 40, "each": 0},
+    "deploy_vaccine": {"initial": 5, "each": 0},
+    "develop_medication": {"initial": 20, "each": 0},
+    "deploy_medication": {"initial": 10, "each": 0},
+    "exert_influence": {"initial": 3, "each": 0},
+    "call_elections": {"initial": 3, "each": 0},
+    "apply_hygienic_measures": {"initial": 3, "each": 0},
+    "launch_campaign": {"initial": 3, "each": 0}
+}
 
 
 def end_round():
-    return json.dumps({"type": "endRound"})
+    return {"type": "endRound"}
 
 
 def put_under_quarantaine(city, rounds):
-    return json.dumps({"type": "putUnderQuarantine", "city": city, "rounds": rounds})
+    return {"type": "putUnderQuarantine", "city": city, "rounds": rounds}
 
 
 def close_airport(city, rounds):
-    return json.dumps({"type": "closeAirport", "city": city, "rounds": rounds})
+    return {"type": "closeAirport", "city": city, "rounds": rounds}
 
 
 def close_connection(from_city, to_city, rounds):
-    return json.dumps({"type": "closeConnection", "fromCity": from_city, "toCity": to_city, "rounds": rounds})
+    return {"type": "closeConnection", "fromCity": from_city, "toCity": to_city, "rounds": rounds}
 
 
 def develop_vaccine(pathogen):
-    return json.dumps({"type": "developVaccine", "pathogen": pathogen})
+    return {"type": "developVaccine", "pathogen": pathogen}
 
 
 def deploy_vaccine(pathogen, city):
-    return json.dumps({"type": "deployVaccine", "pathogen": pathogen, "city": city})
+    return {"type": "deployVaccine", "pathogen": pathogen, "city": city}
 
 
 def develop_medication(pathogen):
-    return json.dumps({"type": "developMedication", "pathogen": pathogen})
+    return {"type": "developMedication", "pathogen": pathogen}
 
 
 def deploy_medication(pathogen, city):
-    return json.dumps({"type": "deployMedication", "pathogen": pathogen, "city": city})
+    return {"type": "deployMedication", "pathogen": pathogen, "city": city}
 
 
 def exert_influence(city):
-    return json.dumps({"type": "exertInfluence", "city": city})
+    return {"type": "exertInfluence", "city": city}
 
 
 def call_elections(city):
-    return json.dumps({"type": "callElections", "city": city})
+    return {"type": "callElections", "city": city}
 
 
 def apply_hygienic_measures(city):
-    return json.dumps({"type": "applyHygienicMeasures", "city": city})
+    return {"type": "applyHygienicMeasures", "city": city}
 
 
 def launch_campaign(city):
-    return json.dumps({"type": "launchCampaign", "city": city})
+    return {"type": "launchCampaign", "city": city}
