c49d601de7844120cc76d122ee47cc4337767b6b
Ruwen <ruwen.sadocco@t-online.de>
2019-12-23 13:48:06 +0100

    Added Pydoc

--- pandemie/util/encoding.py
+++ pandemie/util/encoding.py
@@ -2,4 +2,9 @@ import string
 
 
 def filter_unicode(data):
+    """
+    Function to remove all non-printable characters from a string
+    :param data: str: String to be analysed
+    :return: String with all non-printable characters removed
+    """
     return "".join(c for c in str(data) if c in string.printable)
