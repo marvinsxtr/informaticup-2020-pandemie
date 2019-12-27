1f8b9f41adf6ed8fc6bbaf5113f539d1cdff3d6c
Ruwen <ruwen.sadocco@t-online.de>
2019-12-27 13:25:18 +0100

    Some more documentation of Tester.py

--- pandemie/tester/tester.py
+++ pandemie/tester/tester.py
@@ -134,7 +134,7 @@ class Tester:
 if __name__ == "__main__":
     strategy_name = input("Enter the full name of the strategy you want to test (no .py):\t")
     # TODO Check now if name is valid
-    do_output = input("Should a log be created? (y/n, default=n)\t").lower()
+    do_output = input("Should a log be created? (y/n, default=n):\t").lower()
     do_output = do_output.startswith("y") or do_output.startswith("j")
     visualize = input("Do you want the data of one round to be saved for visualization? (y/n, default=n):\t")
     visualize = visualize.startswith("y") or visualize.startswith("j")
