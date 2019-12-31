792f7425e7ffed0cb3a2cb4ad39865bf55f8b74e
nl <nils@langius.de>
2019-12-31 16:18:16 +0100

    fixed that a wrong score will get printed

--- pandemie/tester/tester.py
+++ pandemie/tester/tester.py
@@ -98,16 +98,18 @@ class Tester:
 
         weighted_sum = 0
         for i, r in enumerate(results):
-            print("Game {0}: {1} after {2} rounds and score: {3:4f}".format(i + 1, r[0], r[1], self.win_weight(r[1])))
 
             if r[0] == "win":
                 self.amount_wins += 1
-                weighted_sum += self.win_weight(r[1])
+                w = self.win_weight(r[1])
             elif r[0] == "loss":
                 self.amount_loss += 1
-                weighted_sum += self.loss_weight(r[1])
+                w = self.loss_weight(r[1])
             else:
                 raise ValueError("Unknown result type {0}".format(r[0]))
+
+            weighted_sum += w
+            print("Game {0}: {1} after {2} rounds and score: {3:4f}".format(i + 1, r[0], r[1], w))
 
         if not self.strategy.silent:
             with open(self.strategy.get_file_path(), "a") as file:
