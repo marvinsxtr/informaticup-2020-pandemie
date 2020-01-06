2bac5350319b9670b563726afc0810813a4cdd01
Marvin Sextro <marvin.sextro@gmail.com>
2020-01-06 17:04:38 +0100

    add is_affordable function

--- pandemie/tester/strategies/final.py
+++ pandemie/tester/strategies/final.py
@@ -152,6 +152,14 @@ class Final(AbstractStrategy):
             return int((round_points - operations.PRICES[identifier]["initial"]) /
                        operations.PRICES[identifier]["each"])
 
+        def is_affordable(identifier):
+            """
+            This function returns whether a measure is affordable
+            :param identifier: operation name
+            :return: whether the measure is affordable
+            """
+            return round_points - operations.PRICES[identifier]["initial"] >= 0
+
         # Used to convert a scale of scores (24 - 4 = 20 where 4 is the minimum points and 20 maximum)
         highest_rating = 24
 
@@ -342,22 +350,24 @@ class Final(AbstractStrategy):
         for pathogen_name in pathogens_names:
             if pathogen_name not in pathogens_medication_available_names and pathogen_name not in \
                     pathogens_medication_in_development_names:
-                rank_operation("develop_medication", pathogen_name, op_score=round(
-                    measure_weights["develop_medication"] * (
-                            pathogens_scores[pathogen_name] +
-                            pathogens_count_infected_cities[pathogen_name]), 5))
+                if is_affordable("develop_medication"):
+                    rank_operation("develop_medication", pathogen_name, op_score=round(
+                        measure_weights["develop_medication"] * (
+                                pathogens_scores[pathogen_name] +
+                                pathogens_count_infected_cities[pathogen_name]), 5))
 
         # Deploy medication in cities at most risk
         for city_name, pathogen_name in cities_pathogen_name.items():
             if pathogen_name in pathogens_medication_available_names:
-                rank_operation("deploy_medication", pathogen_name, city_name, op_score=round(
-                    measure_weights["deploy_medication"] * (
-                            cities_pathogen_score[city_name] +
-                            cities_scores[city_name] +
-                            cities_outbreak_scores[city_name] +
-                            cities_count_flight_connections[city_name] +
-                            pathogens_scores[pathogen_name] +
-                            pathogens_count_infected_cities[pathogen_name]), 5))
+                if is_affordable("deploy_medication"):
+                    rank_operation("deploy_medication", pathogen_name, city_name, op_score=round(
+                        measure_weights["deploy_medication"] * (
+                                cities_pathogen_score[city_name] +
+                                cities_scores[city_name] +
+                                cities_outbreak_scores[city_name] +
+                                cities_count_flight_connections[city_name] +
+                                pathogens_scores[pathogen_name] +
+                                pathogens_count_infected_cities[pathogen_name]), 5))
 
         # Close airports with most difference in risk compared to connected cities
         for city_name in cities_names:
