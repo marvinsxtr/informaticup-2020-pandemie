135a7badbcea5cca060154eeed026cd9bd44efab
Ruwen <ruwen.sadocco@t-online.de>
2020-01-07 20:25:21 +0100

    Added comments to analyse_log.py

--- pandemie/util/analyse_log.py
+++ pandemie/util/analyse_log.py
@@ -3,6 +3,7 @@ from pandemie.util.encoding import filter_unicode
 
 
 def analyse(file):
+    # Init dict vor all known pathogens: Name: [win, loss]
     pathogens = {
         "Admiral Trips": [0, 0],
         "Azmodeus": [0, 0],
@@ -23,21 +24,33 @@ def analyse(file):
         "thisis": [0, 0],
         "Xenomonocythemia": [0, 0]
     }
+
+    # Open the logfile
     with open(file, "r") as f:
         raw_data = f.read()
 
+    # Split different games at $-symbol
     data = raw_data.split("$")
     for d in data:
+        # Check if game was lost
         if "loss" in d[:4]:
+            # Set index for loss
             i = 1
         else:
+            # Set index for win
             i = 0
+        # Split to get the pathogens
         lines = d.split("\n")
+        # Start at 1 -> first pathogen
         for j in range(1, len(lines) - 2):
+            # Load the dict from string
             line = yaml.load(lines[j], Loader=yaml.FullLoader)
             pathogens[filter_unicode(line["name"]).strip()][i] += 1
 
+    # Format the data
     t = "".join(c + "".join(" " for _ in range(31 - len(c))) + "\t-\twins: " + str(pathogens[c][0]) + " - loss: " +
                 str(pathogens[c][1]) + "\n" for c in pathogens)
+
+    # Write the data to the end of the logfile
     with open(file, "a") as f:
         f.write("\n\n" + t)
